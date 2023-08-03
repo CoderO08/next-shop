28056eed88dc6e59c347d8f38226badb854a7d73
Mono439 <mxusanov14@gmail.com>
2023-08-03 21:15:10 +0500

    i aded cards

--- pages/web/index.tsx
+++ pages/web/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./web.module.scss";
 
-async function Products() {}
-const Web = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api");
+  const repo = await res.json();
+  return { props: { repo } };
+};
+const Web = ({ repo }: any) => {
   return (
     <div className={styles.web}>
-      <h1>Web</h1>
+      {repo?.products.map((p: any) => (
+        <div className={styles.card} key={p.id}>
+          <div className={styles.card_image}>
+            <img src={p.img} alt="logo" />
+          </div>
+
+          <div className={styles.card_description}>
+            <p className={styles.text_title}>{p.title}</p>
+            <p className={styles.text_body}>{p.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
